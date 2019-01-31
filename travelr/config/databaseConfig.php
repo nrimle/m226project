b2b359866f13d5c4029dff487f5411161eee5300
Diabetix FE <diabetixfe@gmail.com>
2019-01-31 16:07:06 +0100

    style change, commentable configs, documentation
    * changed arrow color to white
    * made config file commentable for different envs
    * added first version of documentation

--- travelr/config/databaseConfig.php
+++ travelr/config/databaseConfig.php
@@ -5,3 +5,9 @@ return array(
     'password' => 'm226.2019!',
     'database' => 'aeschlic_rimle'
 );
+/*return array(
+    'host' => 'localhost',
+    'username' => 'travelruser',
+    'password' => '123qweasd',
+    'database' => 'travelr'
+);*/
